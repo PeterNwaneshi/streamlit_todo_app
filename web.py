8aee136d3f0ef19f6fee40b4c5611a089d177eb1
ZippaDiva <peter.nwaneshi@gmail.com>
2025-06-22 21:04:03 +0100

    updated web.py

--- web.py
+++ web.py
@@ -12,6 +12,9 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app will increase your productivity")
 
+st.text_input(label='', placeholder='Add new todo ..', 
+              on_change=add_todo, key='New_Todo')
+
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
@@ -20,5 +23,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label='', placeholder='Add new todo ..', 
-              on_change=add_todo, key='New_Todo')
